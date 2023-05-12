b7cfb1be3f2fa2a1c7b4a5cd86a084f3bc217751
Alexander Ulbrich <adulbrich@users.noreply.github.com>
2023-05-12 14:31:59 -0700

    first draft

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), sitemap(), prefetch()],
+  output: "server",
+  adapter: vercel(),
+});
